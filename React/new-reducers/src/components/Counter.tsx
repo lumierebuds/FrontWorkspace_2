d66b7f3011e072be429df67825c8621fe4ffac94
lumierebuds <lumierebuds@gmail.com>
2024-07-24 14:33:52 +0900

    feat:new-reducers(최신 redux 방식)

---
+++ React/new-reducers/src/components/Counter.tsx
@@ -0,0 +1,26 @@
+import { useDispatch, useSelector } from "react-redux"
+import store, { RootState } from "../store/store"
+import { decrement, increByAmount, increment } from "../features/createSlice";
+
+
+
+export default function Counter() {
+
+    const state =  useSelector((state:RootState) => state.counter); 
+    console.log(state);
+    
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button onClick={()=>{dispatch(decrement())}}>-</button>
+            </div>
+            <div>{state.value}</div>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button onClick={()=>{dispatch(increByAmount(2))}}>+</button>
+            </div>
+        </div> 
+    )
+
+}
---
+++ React/new-reducers/src/store/store.ts
@@ -0,0 +1,15 @@
+
+import { configureStore} from "@reduxjs/toolkit";
+import counterSlice from '../features/createSlice';
+
+let store = configureStore({
+    reducer: {
+        counter: counterSlice,
+        // member : memberSlice
+        // board : boardSlice
+    }
+})
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>
